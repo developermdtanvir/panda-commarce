17f2842df6fd92ce9efb53ce16985978a57b3caa
mdtanvirgit <officialtanvir@hotmail.com>
2023-04-10 11:23:07 +0600

    my show constainer is finished

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const container = document.getElementById('backpack-items');
-fetch('data.json')
+const backpackContainer = document.getElementById('backpack-items');
+const showContainer = document.getElementById('show-items')
+fetch('backpack.json')
     .then(res => res.json())
     .then(data => {
         console.log(data)
@@ -13,11 +14,38 @@ fetch('data.json')
                   <div class="card-body">
                     <h5 class="card-title">${element.title}</h5>
                     <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h3>$ ${element.price}</h3>
+                    <button class="panda-btn-buy-now">Bye Now</button>
                   </div>
                 </div>
               </div>
                 `
-
-            container.appendChild(div);
+            backpackContainer.appendChild(div);
         })
     });
+
+
+fetch('show.json')
+    .then(res => res.json())
+    .then(data => {
+        data.shows.forEach(element => {
+            const showDiv = document.createElement('div');
+            showDiv.innerHTML = `
+                <div class="col">
+                <div class="card">
+                <img src=${element.image} class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${element.title}</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h3>$ ${element.price}</h3>
+                    <button class="panda-btn-buy-now">Bye Now</button>
+                </div>
+                </div>
+            </div>
+            `
+            showContainer.appendChild(showDiv)
+
+        })
+    })
+
+
